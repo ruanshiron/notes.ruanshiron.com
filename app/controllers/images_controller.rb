97c67402d04d7244b572251658e3e6e8c5e793c9
ruanshiron <sandt26@gmail.com>
2022-03-16 22:41:28 +0700

    Cài đặp rubocop

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ImagesController < ApplicationController
   def create
     @image = Image.new(attachment: params[:file])
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Note < ApplicationRecord
   with_options presence: true do
     validates :title
